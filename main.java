3ca348c9a46209d3d55d29b6abbe86463f1bb1fb
68050467-rtthpn <68050467@kmitl.ac.th>
2026-08-29 10:14:54 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+public class main {
+// point 1 = pass ; point 2 = goldbuzzer ; point 3 = notpass
+    public static void main(String[] args) {
+        judge j01 = new judge();
+        judge j02 = new judge();
+        judge j03 = new judge();
+        judge j04 = new judge();
+        judge j05 = new judge();
+
+        j01.vote(1);
+        
+
+    }
+}
